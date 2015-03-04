5589c70e5af6f6498d6cef51acbd0a48627f59ab
Nicholas Iebba <nicholasiebba@gmail.com>
2015-03-03 20:37:56 -0500

    Exceptions/IO
    An exercise in Exception Handling and Console IO. Runs in Visual Studio
    2011 and above.

---
+++ ConsoleApplication10/ConsoleApplication10/BetterBinary.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication10
+{
+    //Code by Nicholas Iebba
+    //With some help from MSDN
+    //And from Tutorialspoint.com
+    class BetterBinary
+    {
+        static void Main(string[] args)
+        {
+            BinaryWriter bw;
+            BinaryReader br;
+            int i = 25;
+            double d = 3.14157;
+            bool b = true;
+            string s = "I am doing a make of this program for instructional class on game.";
+            //creates the binary file that stores an int, a doulbe, and a string
+            try
+            {
+                bw = new BinaryWriter(new FileStream("mydata",
+                FileMode.Create));
+            }
+            catch (IOException e) //troubleshoot any possible problems with file creation
+            {
+                Console.WriteLine(e.Message + "\n Cannot create file.");
+                return;
+            }
+            //writing into the file
+            try
+            {
+                bw.Write(i);
+                bw.Write(d);
+                bw.Write(b);
+                bw.Write(s);
+            }
+            catch (IOException e) //troubleshoot any possible problems with writing to file
+            {
+                Console.WriteLine(e.Message + "\n Cannot write to file.");
+                return;
+            }
+
+            bw.Close(); //Close the file (duh)
+            //reading from the file
+            try
+            {
+                br = new BinaryReader(new FileStream("mydata",
+                FileMode.Open));
+            }
+            catch (IOException e) //Try to reopen and catch a possbile error while doing so
+            {
+                Console.WriteLine(e.Message + "\n Cannot open file.");
+                return;
+            }
+            try
+            {
+                i = br.ReadInt32();
+                Console.WriteLine("Integer data: {0}", i);
+                d = br.ReadDouble();
+                Console.WriteLine("Double data: {0}", d);
+                b = br.ReadBoolean();
+                Console.WriteLine("Boolean data: {0}", b);
+                s = br.ReadString();
+                Console.WriteLine("String data: {0}", s);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot read from file.");
+                return;
+            }
+            br.Close();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication11/ConsoleApplication11/TextFile.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace ConsoleApplication11
+{
+    class TextFile
+    {
+        //Code by Nicholas Iebba
+        //With help from MSDN
+        //And Tutorialspoint.com
+        static void Main(string[] args)
+        {
+            StreamWriter sw;
+            StreamReader sr;
+            int i = 57;
+            double p = 48;
+            //bool b = false;
+            //string s = "How is babby formed?";
+            //Create the text file and troublshoot the process
+            try
+            {
+                sw = new StreamWriter(new FileStream("mydata",
+                FileMode.Create));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot create file.");
+                return;
+            }
+            //Writing to the file (as well as troubleshooting the process)
+            try
+            {
+                sw.Write(i);
+                sw.Write(p);
+                //sw.Write(b);
+                //sw.Write(s);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot write to file.");
+                return;
+            }
+
+            sw.Close();
+            //reading from the file
+            try
+            {
+                sr = new StreamReader(new FileStream("mydata",
+                FileMode.Open));
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot open file.");
+                return;
+            }
+            try //Printing the file's info
+            {
+                i = sr.Read();
+                Console.WriteLine("Integer data: {0}", i);
+                p = sr.Read();
+                Console.WriteLine("Double data: {0}", p);
+                //b = sr.ReadBool();
+                //Console.WriteLine("Boolean data: {0}", b);
+                //s = sr.Read();
+                //Console.WriteLine("String data: {0}", s);
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message + "\n Cannot read from file.");
+                return;
+            }
+            sr.Close();
+            Console.ReadKey();
+        }
+    }
+}
